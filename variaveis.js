75502e1440567be57fa2a19f697a8fab679691d8
devScream <138836396+soarestteu@users.noreply.github.com>
2023-08-22 19:33:29 -0300

    Update variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,40 @@
+// VARIAVEIS***************************************************************************** 
+
+// string
+/* 
+    são responsaveis pelo armazenamento de cadeia de caracteres ou textos
+    o operador "=" dentro do javascript, tem a função de atribuição. 
+    pegando o valor declarado do lado direito e atribuindo ao valor do lado esquerdo, no caso o "texto"
+    para atribuir o valor, tem que acrescentar ou aspas duplas ou simples.
+    nesse caso a string 'Curso de JavaScript', passa a ser armazenado dentro da variavel (texto)
+*/
+
+var texto = 'Curso de JavaScript'
+
+// number
+/* 
+    numeros fracionados, sempre tem que separar a fração usando (.)
+    numeros inteiros geralmente são denominados "int"
+    numeros fracionados geralmente são denominados "float"
+*/
+
+var numeroInteiro = -7
+var numeroFracionado = 10.75
+
+// boolean
+/*
+    variaveis booleanas suportam apenas dois valores (true e false)
+*/
+
+var teste = true
+
+
+// cria uma caixa de dialogo
+// alert (texto) 
+
+
+// todos os elementos criados na pagina html estão dentro do objeto (document)
+// document.write (texto)
+
+// executa no console do devTools do navegador, no inspecionar
+//console.log (teste)
